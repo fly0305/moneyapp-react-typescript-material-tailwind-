9b4c8571efb24bdab844b212a02867c1aabdc460
flyasher <flyhighasher@gmail.com>
2021-06-16 10:16:32 +1200

    got rid of config

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -8,11 +8,5 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import {Provider} from "react-redux";
 import store from "./store/store";
 
-
-
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
-
-// f you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
