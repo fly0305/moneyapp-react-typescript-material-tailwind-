951c496694f506d57e614f6bd750fa45ff14096c
flyasher <flyhighasher@gmail.com>
2021-06-16 21:45:51 +1200

    removed contents

--- client/src/pages/main/Account.tsx
+++ client/src/pages/main/Account.tsx
@@ -1,28 +1,13 @@
 import React, { useEffect } from 'react';
-import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router';
 import useAuth from '../../hooks/useAuth';
-import useFeatureFlag from '../../hooks/useFeatureFlag';
-import useUserGroup from '../../hooks/useUserGroup';
-import { logout } from '../../redux/authSlice/thunks/logoutThunk';
-import { RootState, useAppDispatch } from '../../redux/store';
 
 interface AccountProps {}
 
 const Account: React.FC<AccountProps> = () => {
-  const selector = (state: RootState) => ({
-    user: state.auth.user,
-    listingId: state.listing.usersListingId,
-  });
-  const { user, listingId } = useSelector(selector);
-  const dispatch = useAppDispatch();
 
   const history = useHistory();
   const isAuth = useAuth();
-
-  const isAdmin = useUserGroup('administrators');
-
-  const enable_listing_form = useFeatureFlag('enable_listing_form');
 
   useEffect(() => {
     if (!isAuth) {
--- client/src/pages/main/EditAccount.tsx
+++ client/src/pages/main/EditAccount.tsx
@@ -1,8 +1,4 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import { useHistory } from 'react-router-dom';
-import { updateUser } from '../../redux/authSlice/thunks/updateUserThunk';
-import { RootState, useAppDispatch } from '../../redux/store';
 
 interface editAccountFields {
   photo?: string;
@@ -10,31 +6,8 @@ interface editAccountFields {
   lastName?: string;
 }
 
-const EditAccount: React.FC = () => {
-  const dispatch = useAppDispatch();
+const EditAccount: React.FC<editAccountFields> = () => {
 
-  const selector = (state: RootState) => ({
-    photo: state.auth.user?.picture,
-    firstName: state.auth.user?.fistName,
-    lastName: state.auth.user?.lastName,
-  });
-
-  const state: editAccountFields = useSelector(selector);
-  const history = useHistory();
-
-  const handleExit = () => {
-    history.push('/account');
-  };
-
-  const handleSubmit = async (data: editAccountFields) => {
-    Object.entries(data).forEach(([k, newVal]) => {
-      const existingVal = (state as any)[k];
-      if (newVal === existingVal) {
-        delete (data as any)[k];
-      }
-    });
-    dispatch(updateUser(data));
-  };
 
   return (
     <div className={'p-4 sm:w-3/4 lg:w-1/2 sm:mx-auto pt-20'}>
