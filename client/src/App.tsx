aaad32ccb5e2174ea585b82374b20ea7201e5b3d
flyasher <flyhighasher@gmail.com>
2021-06-20 23:34:20 +1200

    auth is working!!!

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,71 @@
-import React from "react";
+import React, { ReactElement } from "react";
 import "./App.css";
 import "./styles/sb-admin-2.min.css";
-import { BrowserRouter as Router, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 import AuthWrapper from "./components/auth/AuthWrapper";
-import Login from "./components/Account/Login";
-import Admin from "./components/Admin/Admin";
-import { PrivateRoute } from "./common/components/PrivateRoute";
-import { AccountRoute } from "./common/components/AccountRoute";
+import { RootState } from "./redux/store"
+import { useSelector } from "react-redux";
+import Dashboard from "./pages/dashboard/Dashboard";
+import User from "./pages/user/User";
+import Listing from "./pages/listing/Listing";
+import Enquiry from "./pages/enquiry/Enquiry";
+import Matching from "./pages/matching/Matching";
+import Content from "./pages/analytics/Content";
+import Performance from "./pages/analytics/Performance";
+import Login from "./pages/auth/Login";
 
-const App: React.FC = () => {
+const App = () => {
+  const selector = (state: RootState) => ({
+    isAuth: state.auth.isAuthenticated,
+    isHydrated: state.auth.isHydrated
+  })
+  const { isAuth, isHydrated } = useSelector(selector);
+
+  interface routeData {
+    path: string | string[];
+    component: ReactElement;
+    exact?: boolean;
+  }
+
+  interface redirectData {
+    to: string;
+    from?: string;
+    exact?: boolean;
+  }
+
+  const renderRoutes = () => {
+    let routes: routeData[] = [];
+    let redirects: redirectData[] = [];
+    if (isAuth) {
+     routes = [
+        { exact: true, path: "/", component: <Dashboard /> },
+        { exact: true, path: "/user", component: <User /> },
+        { exact: true, path: "/listing", component: <Listing /> },
+        { exact: true, path: "/enquiry", component: <Enquiry /> },
+        { exact: true, path: "/matching", component: <Matching /> },
+        { exact: true, path: "/content", component: <Content /> },
+        { exact: true, path: "/performance", component: <Performance /> },
+      ]
+    } else {
+        const routes: routeData[] = [ { exact: true, path: "/login", component: <Login /> } ]      
+      }
+    return (
+      <Switch>
+        {routes.map((r, i) => (
+          <Route key={i} path={r.path} exact={r.exact} render={() => r.component} />
+        ))}
+        {redirects.map((r, i) => (
+          <Redirect key={i} from={r.from} exact={r.exact} to={r.to} />
+        ))}
+      </Switch>
+    )
+  }
   return (
     <div className="App" id="wrapper">
       <AuthWrapper>
         <Router>
-          <Switch>
-            <PrivateRoute path="/">
-              <Admin />
-            </PrivateRoute>
-            <AccountRoute path="/login"><Login /></AccountRoute>
-          </Switch>
+          {isAuth && isHydrated ? <h2>Loading</h2> : ""}
+          {renderRoutes()}
         </Router>
       </AuthWrapper>
     </div>
--- client/src/pages/auth/Login.tsx
+++ client/src/pages/auth/Login.tsx
@@ -1,79 +1,10 @@
 
 import React from 'react';
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
-
-import TextField from '@material-ui/core/TextField';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import CardHeader from '@material-ui/core/CardHeader';
-import Button from '@material-ui/core/Button';
-
-const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-        container: {
-            display: 'flex',
-            flexWrap: 'wrap',
-            width: 400,
-            margin: `${theme.spacing(0)} auto`
-        },
-        loginBtn: {
-            marginTop: theme.spacing(2),
-            flexGrow: 1
-        },
-        header: {
-            textAlign: 'center',
-            background: '#212121',
-            color: '#fff'
-        },
-        card: {
-            marginTop: theme.spacing(10)
-        }
-    })
-);
-
-//state type
-// demo component link: https://github.com/creativesuraj/react-material-ui-login
+import GoogleButton from '../../components/auth/GoogleButton';
 
 const Login: React.FC = () => {
-    const classes = useStyles();
-
     return (
-        <form className={classes.container} noValidate autoComplete="off">
-            <Card className={classes.card}>
-                <CardHeader className={classes.header} title="EasyRent NZ Admin" />
-                <CardContent>
-                    <div>
-                        <TextField
-                            fullWidth
-                            id="username"
-                            type="email"
-                            label="Username"
-                            placeholder="Username"
-                            margin="normal"
-                        />
-                        <TextField
-                            fullWidth
-                            id="password"
-                            type="password"
-                            label="Password"
-                            placeholder="Password"
-                            margin="normal"
-                        />
-                    </div>
-                </CardContent>
-                <CardActions>
-                    <Button
-                        variant="contained"
-                        size="large"
-                        color="secondary"
-                        className={classes.loginBtn}
-                    >
-                        Login
-                    </Button>
-                </CardActions>
-            </Card>
-        </form>
+        <GoogleButton />
     );
 }
 
