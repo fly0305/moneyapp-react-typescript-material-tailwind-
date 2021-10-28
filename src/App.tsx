a5f9dd5e4d7e34fab3abe8e3c13d0602262aaff4
flyasher <flyhighasher@gmail.com>
2021-10-28 15:36:50 +1300

    Setup proper auth flow

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import AuthenticationButton from './components/auth/AuthenticationButton';
+import { useAuth0 } from '@auth0/auth0-react';
+import Dashboard from 'pages/Dashboard';
 
-
-const App = () => {
+const App: React.FC = () => {
+  const { isAuthenticated } = useAuth0();
   return (
     <>
-      <AuthenticationButton />
+      {!isAuthenticated ? <AuthenticationButton /> : <Dashboard />}
     </>
   );
 }
