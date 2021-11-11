861d539d3e6d9a1a93d0d4f07eef5f6c2817d74c
flyasher <flyhighasher@gmail.com>
2021-11-11 21:05:29 +1300

    Got token

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -5,11 +5,21 @@ import AuthenticationButton from '../components/auth/AuthenticationButton';
 interface DashboardProps{};
 
 const Dashboard: React.FC<DashboardProps> = () => {
-    const { user } = useAuth0();
+    let token = '';
+    const { user, getAccessTokenSilently } = useAuth0();
+    const getToken = async () => {
+        const a = await getAccessTokenSilently();
+        console.log(a);
+        token = a;
+    };
+    getToken();
+
+
     return (
         <>
             <h1>Dashboard page</h1>
-            <h2>Welcome back {user?.email}</h2>
+            <h2>Welcome back, {user?.name}</h2>
+            <h2>Email: {user?.email}</h2>
             <AuthenticationButton />
         </>
     )
