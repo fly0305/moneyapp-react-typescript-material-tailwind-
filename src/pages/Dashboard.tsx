3f30621d76d780c553817e91cd8fb6690e704e5a
flyasher <flyhighasher@gmail.com>
2021-11-12 20:29:16 +1300

    Decoded jwt for logging

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -2,32 +2,35 @@ import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import AuthenticationButton from '../components/auth/AuthenticationButton';
 import { Button } from '@mui/material';
+// import jwt_decode from 'jwt-decode';
 
-interface DashboardProps{};
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface DashboardProps {}
 
 const Dashboard: React.FC<DashboardProps> = () => {
-    let token = '';
-    const { user, getAccessTokenSilently } = useAuth0();
-    const getToken = async () => {
-        const a = await getAccessTokenSilently();
-        console.log(a);
-        token = a;
-    };
-    getToken();
+  let token = '';
+  const { user, getAccessTokenSilently } = useAuth0();
+  const getToken = async () => {
+    const a = await getAccessTokenSilently();
+    console.log(a);
+    token = a;
+    // token = jwt_decode(a, { header: true });
+  };
+  getToken();
 
-    const copyToken = async () => {
-        navigator.clipboard.writeText(`${token}`);
-    };
+  const copyToken = async () => {
+    navigator.clipboard.writeText(`${token}`);
+  };
 
-    return (
-        <>
-            <h1>Dashboard page</h1>
-            <h2>Welcome back, {user?.name}</h2>
-            <h2>Email: {user?.email}</h2>
-            <Button onClick={copyToken}>Copy Token</Button>
-            <AuthenticationButton />
-        </>
-    )
-}
+  return (
+    <>
+      <h1>Dashboard page</h1>
+      <h2>Welcome back, {user?.name}</h2>
+      <h2>Email: {user?.email}</h2>
+      <Button onClick={copyToken}>Copy Token</Button>
+      <AuthenticationButton />
+    </>
+  );
+};
 
 export default Dashboard;
