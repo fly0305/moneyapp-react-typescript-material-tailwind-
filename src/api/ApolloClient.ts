c92780f303b6cb68a7ad6cd2b8fa8909b650337e
flyasher <flyhighasher@gmail.com>
2021-12-08 23:45:48 +1300

    Added help article for tmr reading

--- src/api/ApolloClient.ts
+++ src/api/ApolloClient.ts
@@ -6,7 +6,7 @@ import {
 } from '@apollo/client';
 import * as dotenv from 'dotenv';
 dotenv.config({ path: __dirname + '/.env' });
-
+// check this: https://nextsteps.dev/apollo-client-graphQL-and-auth
 const httpLink = new HttpLink({
   uri: process.env.API_URL,
   headers: {
